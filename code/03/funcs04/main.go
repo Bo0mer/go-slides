1c4bcf4925302669f5844b08e9256b39154dad3c
Ivan Borshukov <bo0merzzz@gmail.com>
2017-08-02 10:46:06 +0300

    Add naked return missing example

--- code/03/funcs04/main.go
+++ code/03/funcs04/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+// START OMIT
+func theAnswer() (answer int, err error) {
+	if answer, err = readRemoteAnswer(); err != nil {
+		if !isTemporary(err) {
+			return
+		}
+		answer = -42
+	}
+
+	localAnswer, otherErr := readLocalAnswer()
+	if err != nil {
+		return
+	}
+	answer, err = localAnswer, otherErr
+	return
+}
+
 func main() {
-	// TODO(ivan): Example.
+	fmt.Println(theAnswer())
+}
+
+// END OMIT
+
+func isTemporary(err error) bool { return true }
+
+func readRemoteAnswer() (int, error) {
+	return 0, errors.New("")
+}
+
+func readLocalAnswer() (int, error) {
+	return 42, nil
 }
